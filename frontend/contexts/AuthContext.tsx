e288db084aafd3819fdc0cbc3e14178970a1dcb5
bokuanT <e0701815@u.nus.edu>
2023-09-15 13:59:38 +0800

    Attempt to add cookies to prevent logout on refresh (failed)

--- frontend/contexts/AuthContext.tsx
+++ frontend/contexts/AuthContext.tsx
@@ -1,4 +1,6 @@
-import React, { createContext, ReactNode, useState, useContext } from 'react';
+//Reference: https://www.mikealche.com/software-development/how-to-implement-authentication-in-next-js-without-third-party-libraries
+import React, { createContext, ReactNode, useState, useContext, useEffect } from 'react';
+import Cookies from 'js-cookie';
 
 interface AuthContextType {
     user: string | null;
@@ -11,13 +13,27 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
     const [user, setUser] = useState<string | null>(null);
 
+    useEffect(() => {
+        async function loadUserFromCookies() {
+            const savedUser = Cookies.get('user');
+            console.log("Got a user in the cookies", savedUser)
+            
+            if (savedUser) {
+                setUser(savedUser);
+            }
+        }
+    
+        loadUserFromCookies();
+    }, []);
+
     const login = async (email: string, password: string) => {
         //TODO: implement login
-
+        Cookies.set('user', email, { expires: 1 }); //TODO: expires: 1 is for testing only
         setUser(email);
     };
 
     const logout = () => {
+        Cookies.remove('user');
         setUser(null);
     };
 
